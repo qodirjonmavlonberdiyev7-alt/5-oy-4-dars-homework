7b6123dc47a2a6c45f34fcec900bfe0b6ac7238f
Qodirjon <qodirjonmavlonberdiyev7@gmail.com>
2025-12-23 11:53:42 +0500

    5-oy 5-dars homework

--- schema/author.schema.js
+++ schema/author.schema.js
@@ -5,33 +5,52 @@ const { Schema, model } = require("mongoose");
 const Author = new Schema({
     full_name: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "full_name kiritilishi shart"],
+        unique: false,
+        set: value => value.trim().toLowerCase().split(" ").map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(" "),
+        minLength: [5, "Kamida 5 ta harfdan iborat bo'lishi kerak"],
+        match: [/^[a-zA-Z] +$/, "faqat raqam kiriting"],
     },
     birth_year: {
         type: Number,
-        required:true
+        required:true,
+        max: [new Date().getFullYear() - 25, "Adib kamida 26 yosh bo'lishi kerak"],
+        min: 0
     },
     death_year: {
         type: String,
         required: false,
-        default: null
+        default: null,
+        max: new Date().getFullYear()
     },
     image_url : {
         type: String,
-        required: true
+        required: true,
+        minLength: [15, "url kamida 15 ta belgidan iborat bo'lishi kerak"]
     },
     bio : {
         type: String,
-        required: true
+        required: true,
+        maxLength: 10000,
+        trim: true
     },
     genre : {
         type: String,
-        required: true
+        required: true,
+        toLowerCase: true,
+        enum : {
+            values : ["historical","drama","horror","romance","detective","documantry","science","fiction","fantasy","commedy","reality","animation","trialler","advanture","novel","poetry","satir","mella dramma","action"]
+        },
+        message: `{VALUE} bunday qiymat qabul qilinmaydi`
     },
     period : {
         type: String,
-        required: true
+        required: true,
+        set: value => value.toLowerCase(),
+        enum: {
+            values : ["temuriylar davri", "jadid adabiyoti", "sovet davri", "mustaqillik davri"],
+            message: `{VALUE} bunday qiymat qabul qilinmaydi`
+        }
     },
     creativity: {
         type: String,
